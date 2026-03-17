b07a4a650152b2e9c11ed3419131a044186a3003
hly-22 <lyminhhanh2002@gmail.com>
2026-03-17 17:29:54 +0700

    Reduce maximum image size limit to improve compression efficiency

--- lib/compress-img.ts
+++ lib/compress-img.ts
@@ -4,8 +4,9 @@ export async function compressImageFile(file: File): Promise<File> {
     if (file.type === "application/pdf") return file;
 
     return await imageCompression(file, {
-        maxSizeMB: 2,
+        maxSizeMB: 1,
         maxWidthOrHeight: 1920,
+        initialQuality: 0.8,
         useWebWorker: true,
     })
 }
