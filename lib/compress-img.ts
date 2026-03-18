6c1dd7ab6ae04856ba40c26a104faa51c74052b3
hly-22 <lyminhhanh2002@gmail.com>
2026-03-18 18:04:51 +0700

    Refactor image compression function to store compressed file in a variable before returning

--- lib/compress-img.ts
+++ lib/compress-img.ts
@@ -3,10 +3,12 @@ import imageCompression from "browser-image-compression";
 export async function compressImageFile(file: File): Promise<File> {
     if (file.type === "application/pdf") return file;
 
-    return await imageCompression(file, {
+    const compressed = await imageCompression(file, {
         maxSizeMB: 1,
         maxWidthOrHeight: 1920,
         initialQuality: 0.8,
         useWebWorker: true,
     })
+
+    return new File([compressed], file.name, { type: compressed.type });
 }
